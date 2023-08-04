fcb6ffae39646673dd2491dfca50c32405991e1b
mayingi Brandon <banabato@outlook.fr>
2023-08-04 16:47:38 +0200

    first commit

---
+++ assets/script/script.js
@@ -0,0 +1,15 @@
+  // Fonction pour afficher ou masquer le menu déroulant pour mobile en fonction de la largeur de l'écran
+  function toggleMobileMenu() {
+    const menuMobile = document.getElementById("menu-mobile");
+    if (window.innerWidth <= 817) {
+      menuMobile.style.display = "block";
+    } else {
+      menuMobile.style.display = "none";
+    }
+  }
+
+  // Appeler la fonction au chargement de la page
+  toggleMobileMenu();
+
+  // Écouter les changements de taille d'écran et réagir en conséquence
+  window.addEventListener("resize", toggleMobileMenu);
