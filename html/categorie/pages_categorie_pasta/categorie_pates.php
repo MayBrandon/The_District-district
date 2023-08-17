5d0e16065333f257b8ee97d3dd57af323d123419
mayingi Brandon <banabato@outlook.fr>
2023-08-17 13:24:42 +0200

    categorie salade

--- html/categorie/pages_categorie_pasta/categorie_pates.php
+++ html/categorie/pages_categorie_pasta/categorie_pates.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Categorie - Pasta</title>
     <link rel="stylesheet" href="/assets/css/categorie_css/categorie-plat.css" />
     <link rel="icon" type="image/png" href="/assets/img/branding/favicon.png" />
 </head>
