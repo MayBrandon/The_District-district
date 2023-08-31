0a70b4ca4e88d58a388962329da0dbaf08596fe7
mayingi Brandon <brandon.mayingi.pro@outlook.fr>
2023-08-31 18:39:05 +0200

    cs

---
+++ html/panier.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="f">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/css/style.css" />
+    <link rel="icon" type="image/png" href="/assets/img/branding/favicon.png" />
+    <title>The District - Panier</title>
+</head>
+<body>
+    <?php include('../header_footer/header.php') ?>
+
+</body>
+</html>
