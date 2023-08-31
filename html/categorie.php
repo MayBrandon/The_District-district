f25947bd42d41ef7ff61a518e85df2d7ba36ce9a
mayingi Brandon <brandon.mayingi.pro@outlook.fr>
2023-08-31 18:41:15 +0200

    cs

--- html/categorie.php
+++ html/categorie.php
@@ -111,12 +111,9 @@
     </div> -->
   </div>
 
-  <footer class="social-media">
-    <div class="facebook"><img class="social-logo" src="/assets/img/social/facebook.png" alt=""></div>
-    <div class="insta"><img class="social-logo" src="/assets/img/social/instagram.png" alt=""></div>
-    <div class="linkedin"><img class="social-logo" src="/assets/img/social/linkedin.png" alt=""></div>
-    <div class="pinterest"><img class="social-logo" src="/assets/img/social/pinterest.png" alt=""></div>
-  </footer>
+  <?php
+      include('../header_footer/footer.php'); 
+  ?> 
 </body>
 
 </html>
--- html/categorie/pages_categorie_burger/categorie_burger_2.php
+++ html/categorie/pages_categorie_burger/categorie_burger_2.php
@@ -97,12 +97,5 @@
 
       <?php include('../../../header_footer/footer.php') ?>
 
-
-      <!-- <footer class="social-media">
-        <div class="facebook"><img class="social-logo" src="/assets/img/social/facebook.png" alt=""></div>
-        <div class="insta"><img class="social-logo" src="/assets/img/social/instagram.png" alt=""></div>
-        <div class="linkedin"><img class="social-logo" src="/assets/img/social/linkedin.png" alt=""></div>
-        <div class="pinterest"><img class="social-logo" src="/assets/img/social/pinterest.png" alt=""></div>
-      </footer> -->
 </body>
 </html>
